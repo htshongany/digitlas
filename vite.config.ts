34da56eab19c852b8ba4419ebeaff320c1cfdceb
htshongany <htshongany@gmail.com>
2025-10-04 07:42:36 -0800

    fix: Configure base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/digitlas/', 
+  base: '/', 
   server: {
     port: 3000,
     host: '0.0.0.0',
